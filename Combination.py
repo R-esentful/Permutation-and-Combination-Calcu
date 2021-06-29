71f35bd53e620476e7f9471491c23888711e5d72
Resentful <raneljohnt@gmail.com>
2021-06-29 18:53:47 +0800

    Permutation and Combination

---
+++ Combination.py
@@ -0,0 +1,34 @@
+def Combination(number,r):
+
+    nfactorial = []
+    rfactorial = []
+
+    temp = number - r
+    tempnf = 1
+    tempnr = 1
+
+    total= 0
+
+    if number >r : 
+        for a in range(1,number + 1):
+            nfactorial.append(a)
+            for a in nfactorial[::-1]:
+                if a == temp:
+                    nfactorial.remove(a)
+                    temp = temp -1
+        for b in nfactorial:
+            tempnf = tempnf * b
+
+        for c in range(1,r+1):
+            rfactorial.append(c)
+        
+        for d in rfactorial:
+            tempnr = tempnr * d
+        
+        total = tempnf / tempnr
+
+        print(total)
+    else:
+        print("Input is incorrect!")
+
+Combination(10,2)
---
+++ Permutation.py
@@ -0,0 +1,25 @@
+
+from ast import Num
+
+
+def Permutation(number,r):
+
+    nfactorial = []
+    temp = number - r
+    total = 1 
+
+    if number >r : 
+        for x in range(1,number + 1):
+            nfactorial.append(x)
+            for y in nfactorial[::-1]:
+                if y == temp:
+                    nfactorial.remove(y)
+                    temp = temp -1
+
+        for z in nfactorial:
+            total = total * z        
+    else:
+        print("Input is incorrect!")
+
+    
+  
